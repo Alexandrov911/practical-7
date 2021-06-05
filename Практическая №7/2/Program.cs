99ce182b4e6fdf0f9376fbd7326eab57cc9eed93
Alexandrov911 <sashok17062002@mail.ru>
2021-06-05 09:27:59 +0300

    Практическая 7

---
+++ Практическая №7/2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину массива n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину массива b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[n];
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = r.Next(-20,20);
+                Console.WriteLine(mas[i]);
+            }
+            Console.WriteLine();
+            
+            for (int i = 0; i < n; i++)
+            {
+                mas[i]= mas[i] * b;
+            }
+            Console.WriteLine();
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine("otvet " + mas[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Практическая №7/Практическая №7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая__7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[n];
+            int sum = 0;
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = r.Next(-20, 20);
+                Console.WriteLine(mas[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            { 
+                   sum = sum + mas[i];  
+            }
+            Console.WriteLine("otvet " +sum);
+
+            Console.ReadKey();
+        }
+    }
+}
